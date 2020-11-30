c8cc6a6a7e32bac06c05caa49cd9c17f7e2614c6
Kreuzritterrr <75267598+Kreuzritterrr@users.noreply.github.com>
2020-11-30 19:34:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Введите x для вычисления y=ln(lg(e^x))-0.35\nx=";
+            Console.Write(a);
+            double x = double.Parse(Console.ReadLine());
+            double y = Math.Log(Math.Log10(Math.Exp(x))) - 0.35;
+            Console.Write("y=" + y);
+            Console.ReadKey();
+        }
+    }
+}
